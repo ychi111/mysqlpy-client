874104dfe54dd1fb2e2a32cbb507763e966d8eaa
sputnik5459 <37472747+sputnik5459@users.noreply.github.com>
2018-12-17 12:05:48 +0300

    Update and rename client.py
    client.py was devided into two files: GUI.py and build_client.py

--- client.py
+++ client.py
@@ -158,19 +158,4 @@ class RegWindow(Win):
         else:
             windows["RegWindow"].labels["error"].config(text = "Passwords are not equal!")
 
-
-
-#Start
 windows = {}
-
-AutWindow = AutWindow()
-RegWindow = RegWindow()
-AdminWindow = AdminWindow()
-UserWindow = UserWindow()
-
-windows["AutWindow"] = AutWindow
-windows["RegWindow"] = RegWindow
-windows["AdminWindow"] = AdminWindow
-windows["UserWindow"] = UserWindow
-
-AutWindow.build()
